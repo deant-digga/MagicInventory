6598e1e0398c169379dfcfe87ab2cc9eb44ab23c
deant <deant@DEANT-PC>
2017-06-05 08:22:15 +1000

    Add project files.

---
+++ MagicInventory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
